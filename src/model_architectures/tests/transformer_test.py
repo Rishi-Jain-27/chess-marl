fdc600c0a0cb67b0ea695b72e400e70149dc2871
Rishi Jain <27rishijainpersonalemail@gmail.com>
2026-06-14 15:34:39 -0400

    Fixed issues with masks in all the tests + added asserts

--- src/model_architectures/tests/transformer_test.py
+++ src/model_architectures/tests/transformer_test.py
@@ -1,7 +1,7 @@
-from src.model_architectures.transformer import ActorCritic, compute_gae
+from src.model_architectures.cnn import ActorCritic, compute_gae
 import torch
+import numpy as np
 
-# Stole this from hybrid_test.py, should be fine
 if __name__ == '__main__':
     # Test model init
     test_model = ActorCritic((8, 8, 111), 4672)
@@ -12,8 +12,11 @@ if __name__ == '__main__':
     # 1 for the batch dim
     test_state_forward = torch.randn((1, 8, 8, 111), dtype=torch.float32)
 
-    action_mask = [1 for _ in range(4672)] # 1 == legal
+    action_mask = np.ones(4672, dtype=np.int64)
+    action_mask[[4, 5, 6]] = 0
     logits, forward_value = test_model(test_state_forward, action_mask)
+    assert logits.shape == (1, 4672)
+    assert forward_value.shape == (1,)
 
     print(f"Logits: {logits}")
     print(f"Value: {forward_value}")
@@ -23,6 +26,7 @@ if __name__ == '__main__':
 
     test_state_select_action = torch.randn((8, 8, 111), dtype=torch.float32)
     action, log_prob, select_action_value = test_model.select_action(test_state_select_action, action_mask)
+    assert select_action_value.shape == (1,)
 
     print(f"Action: {action}")
     print(f"Action's Log Prob: {log_prob}")
@@ -32,12 +36,13 @@ if __name__ == '__main__':
     torch.manual_seed(42)
 
     test_eval_states = torch.randn((1, 8, 8, 111), dtype=torch.float32)
-    test_eval_actions = torch.ones((1, 8, 8, 111), dtype=torch.float32)
+    test_eval_actions = torch.tensor([5]) # the new log prob should be nuked bc 5 is in action mask
 
     # gives (1, 4672) shape — 1 is batch dim, this is good!
-    old_act_masks = torch.tensor(action_mask).unsqueeze(0)
+    old_act_masks = action_mask
 
     new_log_probs, entropy, values = test_model.evaluate_actions(test_eval_states, test_eval_actions, old_act_masks)
+    assert new_log_probs.shape == (1,)
 
     print(f"New log probs: {new_log_probs}")
     print(f"Entropy: {entropy}")
